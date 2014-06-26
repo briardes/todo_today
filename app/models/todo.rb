ec74bc0faf3c97353fba226304dfc16c7ebdf329
Clinton N. Dreisbach <clinton@dreisbach.us>
2014-06-26 15:42:41 -0400

    Simplify logic
    When you find yourself writing:
    
    ```ruby
    if condition
      true
    else
      false
    end
    ```
    
    you can erase the whole if statement. Remember, if runs the first branch
    if the condition is true. In this case, that makes it return true, which
    is what the conditional is already returning.
    
    `completed == false` is only necessary if you want to ensure complete
    isn't nil. Otherwise, `!completed` is the better way to do this.

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,11 +1,8 @@
 class Todo < ActiveRecord::Base
   belongs_to :list
   has_one :user, through: :list
+
   def late?
-    if DateTime.now > self.due_date && self.completed == false
-      true
-    else
-      false
-    end
+    DateTime.now > due_date && !completed
   end
 end
